684207c6534b601fe7ebb050e73c9715a5466620
Kristjan Rajaniemi <kristjan.rajaniemi@live.fi>
2023-08-19 16:40:33 +0300

    split into modules, test chess board is properly setup

---
+++ src/chess.rs
@@ -0,0 +1,52 @@
+use crate::square::Square;
+
+#[derive(Debug)]
+pub struct Chess {
+    pub board: Vec<Vec<Square>>,
+}
+
+impl Chess {
+    pub fn new() -> Chess {
+        let mut board: Vec<Vec<Square>> = Vec::new();
+
+        for i in 0..8 {
+            let mut row: Vec<Square> = Vec::new();
+            for j in 0..8 {
+                let sq = Square::new(i, j);
+                row.push(sq);
+            }
+            board.push(row);
+        }
+
+        if board.len() != 8 {
+            panic!("Invalid board");
+        }
+        for board_row in board.iter() {
+            if board_row.len() != 8 {
+                panic!("Invalid board");
+            }
+        }
+
+        Chess { board }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn chess_board_is_proper() {
+        let chess_board: Chess = Chess::new();
+        assert_eq!(chess_board.board[0][0].name, "A1");
+        assert_eq!(chess_board.board[0][7].name, "A8");
+        assert_eq!(chess_board.board[1][1].name, "B2");
+        assert_eq!(chess_board.board[2][2].name, "C3");
+        assert_eq!(chess_board.board[3][3].name, "D4");
+        assert_eq!(chess_board.board[4][4].name, "E5");
+        assert_eq!(chess_board.board[5][5].name, "F6");
+        assert_eq!(chess_board.board[6][6].name, "G7");
+        assert_eq!(chess_board.board[7][7].name, "H8");
+        assert_eq!(chess_board.board[4][2].name, "E3");
+        assert_eq!(chess_board.board[7][0].name, "H1");
+    }
+}
---
+++ src/square.rs
@@ -0,0 +1,38 @@
+#[derive(Clone, Debug)]
+pub struct Square {
+    pub file: u8,
+    pub row: u8,
+    pub name: String,
+}
+
+impl Square {
+    pub fn new(file: u8, row: u8) -> Square {
+        let file_string: String = match file {
+            0 => String::from("A"),
+            1 => String::from("B"),
+            2 => String::from("C"),
+            3 => String::from("D"),
+            4 => String::from("E"),
+            5 => String::from("F"),
+            6 => String::from("G"),
+            7 => String::from("H"),
+            _ => panic!("Invalid file"),
+        };
+        let row_string: String = match row {
+            0 => String::from("1"),
+            1 => String::from("2"),
+            2 => String::from("3"),
+            3 => String::from("4"),
+            4 => String::from("5"),
+            5 => String::from("6"),
+            6 => String::from("7"),
+            7 => String::from("8"),
+            _ => panic!("Invalid row"),
+        };
+        Square {
+            file,
+            row,
+            name: String::from(file_string + &row_string),
+        }
+    }
+}
