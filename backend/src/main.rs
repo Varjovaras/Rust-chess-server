ec463a396bed0203a5e7315db7d4935499fa6ef1
Kristjan Rajaniemi <kristjan.rajaniemi@live.fi>
2023-10-19 18:55:19 +0300

    change backend to axum and clippy fixes

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -7,57 +7,52 @@ mod gamestate;
 mod moves;
 mod piece;
 mod player;
-use std::io;
 
-use actix_cors::Cors;
-use actix_web::{
-    get, http::header, middleware::Logger, post, App, HttpResponse, HttpServer, Responder,
+use axum::{
+    http::{Method, StatusCode},
+    response::IntoResponse,
+    routing::{get, post},
+    Router,
 };
+use shuttle_axum::ShuttleAxum;
+// use tower::{Service, ServiceBuilder, ServiceExt};
+use tower_http::cors::{any, CorsLayer};
 
 // use actix_web::{get, web::ServiceConfig, HttpResponse, Responder};
 
 use crate::chess::Chess;
-
 //cargo watch -qcx 'shuttle run'
 
-#[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
+async fn hello_world() -> &'static str {
+    "Hello, world!"
 }
 
-#[get("/chess")]
-async fn get_chess() -> impl Responder {
-    HttpResponse::Ok().body(serde_json::to_string(&Chess::new_starting_position()).unwrap())
+async fn chess() -> String {
+    serde_json::to_string(&Chess::new_starting_position()).unwrap()
 }
 
-// async fn manual_hello() -> impl Responder {
-//     HttpResponse::Ok().body("Hey there!")
-// }
+async fn chess_move() -> impl IntoResponse {
+    let mut chess = Chess::new();
+    chess.starting_position();
+    chess.make_move_from_str("e2", "e4");
+    (
+        StatusCode::OK,
+        serde_json::to_string(&Chess::new_starting_position()).unwrap(),
+    )
+}
 
-#[actix_web::main]
-async fn main() -> io::Result<()> {
-    // env_logger::init_from_env(env_logger::Env::default().default_filter_or("info"));
+#[shuttle_runtime::main]
+async fn axum() -> ShuttleAxum {
+    let cors = CorsLayer::new()
+        // allow `GET` and `POST` when accessing the resource
+        .allow_methods([Method::GET, Method::POST])
+        // allow requests from any origin
+        .allow_origin(any());
+    let router = Router::new()
+        .route("/", get(hello_world))
+        .route("/chess", get(chess))
+        .route("/chess", post(chess_move))
+        .layer(cors);
 
-    // log::info!("starting HTTP server at http://localhost:8080");
-
-    HttpServer::new(move || {
-        App::new()
-            .wrap(
-                Cors::default()
-                    .allowed_origin("http://localhost:5173")
-                    .allowed_methods(vec!["GET", "POST"])
-                    .allowed_headers(vec![header::AUTHORIZATION, header::ACCEPT])
-                    .allowed_header(header::CONTENT_TYPE)
-                    .supports_credentials()
-                    .max_age(3600),
-            )
-            .service(hello)
-            .service(get_chess)
-            .wrap(Logger::default())
-        // .service(user::info)
-    })
-    .bind(("127.0.0.1", 8000))?
-    .workers(2)
-    .run()
-    .await
+    Ok(router.into())
 }
