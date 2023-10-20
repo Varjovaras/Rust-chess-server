e32ee2229eeeae58f5ae0c3e398d553a40818d08
Kristjan Rajaniemi <kristjan.rajaniemi@live.fi>
2023-10-20 12:03:21 +0300

    remove unused folder

--- my-app/src/routes/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-	export let data;
-	console.log(data.post.board[0]);
-	type Chess = typeof data;
-</script>
-
-<h1>Welcome to SvelteKit</h1>
-<p>{data.post.board[0][0]}</p>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
