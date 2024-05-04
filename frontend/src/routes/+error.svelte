87164ae2975e92fa72cf9fe8f4d316fbb2627e61
Kristjan Rajaniemi <kristjan.rajaniemi@live.fi>
2024-05-05 01:47:17 +0300

    remove unused function in +error.svelte

--- frontend/src/routes/+error.svelte
+++ frontend/src/routes/+error.svelte
@@ -4,18 +4,6 @@
 	console.log($page.error);
 
 	export let apiUrl: string;
-
-	const fun = async () => {
-		try {
-			console.log('Resetting board');
-			const response = await fetch(`${apiUrl}/api/chess`);
-			const data = await response.json();
-			const validatedChess: Chess = chessSchema.parse(data.chess);
-			return validatedChess;
-		} catch (error) {
-			console.error(error);
-		}
-	};
 </script>
 
 <h1 class="text-indigo-50">{$page.status}: {$page.error?.message}</h1>
