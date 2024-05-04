98dc18292fa6ad8a7e33df087bedddd8513abf30
Kristjan Rajaniemi <kristjan.rajaniemi@live.fi>
2024-05-05 01:47:39 +0300

    remove other unused stuff in +error svelte

--- frontend/src/routes/+error.svelte
+++ frontend/src/routes/+error.svelte
@@ -1,9 +1,6 @@
 <script lang="ts">
 	import { page } from '$app/stores';
-	import { chessSchema, type Chess } from '$lib/types';
 	console.log($page.error);
-
-	export let apiUrl: string;
 </script>
 
 <h1 class="text-indigo-50">{$page.status}: {$page.error?.message}</h1>
