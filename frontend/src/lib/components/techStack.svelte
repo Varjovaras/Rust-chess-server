152bde6ec5d870a6410b5429a6e71d44206ad5d2
Kristjan Rajaniemi <kristjan.rajaniemi@live.fi>
2024-05-14 11:27:18 +0300

    add small todolist

--- frontend/src/lib/components/techStack.svelte
+++ frontend/src/lib/components/techStack.svelte
@@ -6,4 +6,16 @@
 		Built with Rust on the backend and Sveltekit, Typescript and Tailwindcss on
 		the frontend
 	</p>
+	<ul class="m-4">
+		todo:
+		<div class="text-xs">
+			<li>
+				Improve the smoothness of making a move by utilizing a WebSocket
+				connection instead of POST requests.
+			</li>
+			<li>Allow playing versus other players</li>
+			<li>Make dragging a piece on mobile smoother</li>
+			<li>Make a game analysis tool using stockfish</li>
+		</div>
+	</ul>
 </div>
