360f969a4ebc341b54576ecfa5d079469e9fdf02
Kristjan Rajaniemi <kristjan.rajaniemi@live.fi>
2024-07-02 11:07:11 +0300

    new cargo-shuttle version

--- frontend/src/routes/+error.svelte
+++ frontend/src/routes/+error.svelte
@@ -1,6 +1,10 @@
 <script lang="ts">
-	import { page } from '$app/stores';
-	console.log($page.error);
+    import { page } from "$app/stores";
+    console.log($page.error);
 </script>
 
 <h1 class="text-indigo-50">{$page.status}: {$page.error?.message}</h1>
+<div>
+    if backend is not online, it is probably idle on shuttle.rs servers. It
+    takes a few seconds to a minute to boot up
+</div>
