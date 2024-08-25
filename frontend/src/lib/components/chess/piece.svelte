49c2d4420fda61534a62ad93ea8eec447d7f01c3
Kristjan Rajaniemi <kristjan.rajaniemi@live.fi>
2024-08-25 14:48:52 +0300

    changes to screen size scaling

--- frontend/src/lib/components/chess/piece.svelte
+++ frontend/src/lib/components/chess/piece.svelte
@@ -1,19 +1,32 @@
 <script lang="ts">
-import { returnCorrectPieceColor } from "$lib/components/chess/utils";
-import type { Square } from "$lib/types";
-export let sq: Square;
+    import { returnCorrectPieceColor } from "$lib/components/chess/utils";
+    import type { Square } from "$lib/types";
+    export let sq: Square;
+
+    $: pieceSize = `
+  w-[11vw] h-[11vw]
+  sm:w-[5vw] sm:h-[5vw]
+  md:w-[4vw] md:h-[4vw]
+  lg:w-[3vw] lg:h-[3vw]
+  xl:w-[2.5vw] xl:h-[2.5vw]
+  min-w-6 min-h-6
+  max-w-16 max-h-16
+`;
 </script>
 
-<div>
-	{#if sq.piece === 'None'}
-		<div id={`${sq.file.toLowerCase()}${sq.rank + 1} no piece`} />
-	{:else}
-		<img
-			class="mx-auto block max-w-full w-full h-full"
-			src={returnCorrectPieceColor(sq.piece)}
-			alt={'square with a piece'}
-			draggable="true"
-			id={`${sq.file.toLowerCase()}${sq.rank + 1} ${sq.piece}`}
-		/>
-	{/if}
+<div class="w-full h-full flex items-center justify-center">
+    {#if sq.piece === "None"}
+        <div
+            id={`${sq.file.toLowerCase()}${sq.rank + 1} no piece`}
+            class={pieceSize}
+        />
+    {:else}
+        <img
+            class={`object-contain ${pieceSize}`}
+            src={returnCorrectPieceColor(sq.piece)}
+            alt={`${sq.piece} piece`}
+            draggable="true"
+            id={`${sq.file.toLowerCase()}${sq.rank + 1} ${sq.piece}`}
+        />
+    {/if}
 </div>
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,77 +1,80 @@
 <script lang="ts">
-import Chessboard from "$lib/components/chess/chessboard.svelte";
-import { startingPosition } from "$lib/components/chess/startingPosition";
-import ErrorMessage from "$lib/components/errorMessage.svelte";
-import ResetButton from "$lib/components/resetButton.svelte";
-import { type Chess, chessSchema } from "$lib/types";
-import { type ModalSettings, getModalStore } from "@skeletonlabs/skeleton";
-import type { PageData } from "./$types";
+    import Chessboard from "$lib/components/chess/chessboard.svelte";
+    import { startingPosition } from "$lib/components/chess/startingPosition";
+    import ErrorMessage from "$lib/components/errorMessage.svelte";
+    import ResetButton from "$lib/components/resetButton.svelte";
+    import { type Chess, chessSchema } from "$lib/types";
+    import { type ModalSettings, getModalStore } from "@skeletonlabs/skeleton";
+    import type { PageData } from "./$types";
 
-const modalStore = getModalStore();
+    const modalStore = getModalStore();
 
-const whiteModal: ModalSettings = {
-	type: "alert",
-	// Data
-	title: "White won!",
-	body: "White won!",
-};
+    const whiteModal: ModalSettings = {
+        type: "alert",
+        // Data
+        title: "White won!",
+        body: "White won!",
+    };
 
-const blackModal: ModalSettings = {
-	type: "alert",
-	// Data
-	title: "Black won!",
-	body: "Black won!",
-};
+    const blackModal: ModalSettings = {
+        type: "alert",
+        // Data
+        title: "Black won!",
+        body: "Black won!",
+    };
 
-export let data: PageData;
-let chess = data.data.chess;
-const errorMessage = "";
-const apiUrl = data.data.url;
+    export let data: PageData;
+    let chess = data.data.chess;
+    const errorMessage = "";
+    const apiUrl = data.data.url;
 
-const handleMove = async (startSq: string, endSq: string) => {
-	0;
-	console.log(`Move from ${startSq} to ${endSq}`);
-	try {
-		chess = await fetchMove(startSq, endSq);
-		if (chess.white_player.victory) {
-			modalStore.trigger(whiteModal);
-		} else if (chess.black_player.victory) {
-			modalStore.trigger(blackModal);
-		}
-		// removeHoverEffect(); // Remove hover effect from all buttons after the move
-	} catch (error) {
-		console.error(error);
-	}
-};
+    const handleMove = async (startSq: string, endSq: string) => {
+        0;
+        console.log(`Move from ${startSq} to ${endSq}`);
+        try {
+            chess = await fetchMove(startSq, endSq);
+            if (chess.white_player.victory) {
+                modalStore.trigger(whiteModal);
+            } else if (chess.black_player.victory) {
+                modalStore.trigger(blackModal);
+            }
+            // removeHoverEffect(); // Remove hover effect from all buttons after the move
+        } catch (error) {
+            console.error(error);
+        }
+    };
 
-const fetchMove = async (startSq: string, endSq: string): Promise<Chess> => {
-	const newMove: [string, string] = [startSq, endSq];
-	const response = await fetch(`${apiUrl}/api/chess`, {
-		method: "POST",
-		headers: {
-			"Content-Type": "application/json",
-		},
-		body: JSON.stringify({
-			list_of_moves: chess.list_of_moves,
-			new_move: newMove,
-		}),
-	});
-	const data = await response.json();
-	const validatedChess = chessSchema.parse(data.chess);
-	if (chess.board === validatedChess.board) {
-		console.log("Move was not possible");
-	}
-	return validatedChess;
-};
+    const fetchMove = async (
+        startSq: string,
+        endSq: string,
+    ): Promise<Chess> => {
+        const newMove: [string, string] = [startSq, endSq];
+        const response = await fetch(`${apiUrl}/api/chess`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                list_of_moves: chess.list_of_moves,
+                new_move: newMove,
+            }),
+        });
+        const data = await response.json();
+        const validatedChess = chessSchema.parse(data.chess);
+        if (chess.board === validatedChess.board) {
+            console.log("Move was not possible");
+        }
+        return validatedChess;
+    };
 
-const handleReset = () => {
-	console.log("Resetting game");
-	chess = startingPosition;
-};
+    const handleReset = () => {
+        console.log("Resetting game");
+        chess = startingPosition;
+    };
 </script>
 
-<div class="flex flex-col p-8">
-	<ErrorMessage {errorMessage} />
-	<Chessboard {chess} {handleMove} />
-	<ResetButton {handleReset} />
+<div class="flex flex-col w-screen p-8">
+    <ErrorMessage {errorMessage} />
+    <Chessboard {chess} {handleMove} />
+    <ResetButton {handleReset} />
 </div>
