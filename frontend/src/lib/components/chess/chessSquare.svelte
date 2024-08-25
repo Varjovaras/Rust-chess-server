7be8ffc56e03d9243c2cc3bc58c5201e986a23bf
Kristjan Rajaniemi <kristjan.rajaniemi@live.fi>
2024-08-25 13:48:04 +0300

    make chess square into its own component

--- frontend/src/lib/components/chess/chessSquare.svelte
+++ frontend/src/lib/components/chess/chessSquare.svelte
@@ -1,40 +1,63 @@
 <script lang="ts">
-import type { PossibleMoves, Square } from "$lib/types";
+import type { Chess, PossibleMoves, Square as SquareType } from "../../types";
 import Piece from "./piece.svelte";
-type Coordinates = {
-	i: number;
-	j: number;
-};
 
-export let sq: Square;
+export let chess: Chess;
+export let sq: SquareType;
 export let selectedButton: string | null;
 export let possibleMoves: PossibleMoves;
-export let handleDragStart: (sq: Square) => void;
+export let handleClick: (sq: SquareType) => void;
+export let handleDragStart: (sq: SquareType) => void;
 export let handleDrop: (event: DragEvent) => void;
-export let handleClick: (sq: Square) => Promise<void>;
-export let handleTouchStart: (event: TouchEvent, sq: Square) => void;
+export let handleTouchStart: (event: TouchEvent, sq: SquareType) => void;
 export let handleTouchMove: (event: TouchEvent) => void;
 export let handleTouchEnd: (event: TouchEvent) => void;
-export let buttonClass: string;
-export const draggable = true;
-export let coordinates: Coordinates;
+
+$: squareId = `${sq.file.toLowerCase()}${sq.rank + 1}`;
+$: isSelected = selectedButton === squareId;
+$: isPossibleMove = possibleMoves.some(
+	(move) => move[1][0] === sq.file.charCodeAt(0) - 65 && move[1][1] === sq.rank,
+);
+$: isKingInCheck =
+	typeof sq.piece === "object" &&
+	sq.piece.King !== undefined &&
+	((sq.piece.King === "White" && chess.white_player.in_check) ||
+		(sq.piece.King === "Black" && chess.black_player.in_check));
+
+$: squareColor = sq.color === "White" ? "bg-gray-200" : "bg-gray-400";
+$: hoverColor = "hover:bg-gray-600";
+$: checkColor = isKingInCheck ? "bg-red-800 hover:bg-red-900" : "";
+
+$: squareClass = `
+    h-11 w-11 sm:h-14 sm:w-14 md:h-16 md:w-16 lg:h-18 lg:w-18 xl:w-20 xl:h-20 
+    ${squareColor} ${hoverColor} ${checkColor} text-center
+    ${isSelected ? "selected" : ""}
+    ${isPossibleMove ? "possible_move" : ""}
+  `;
+
+$: draggable = sq.piece !== "None";
 </script>
 
 <button
-  class={buttonClass}
-  class:selected={selectedButton === sq.file.toLowerCase() + (sq.rank + 1)}
-  class:possible_move={possibleMoves.some(
-    (move) => move[1][0] === coordinates.j && move[1][1] === 8 - coordinates.i
-  )}
-  {draggable}
-  on:dragstart={() => handleDragStart(sq)}
-  on:dragover|preventDefault
-  on:drop|preventDefault={handleDrop}
-  id={`${sq.file.toLowerCase()}${sq.rank + 1} ${sq.piece}`}
-  on:click|preventDefault={() => handleClick(sq)}
-  on:touchstart|preventDefault={(event) => handleTouchStart(event, sq)}
-  on:touchmove|preventDefault={handleTouchMove}
-  on:touchend|preventDefault={handleTouchEnd}
+	class={squareClass}
+	{draggable}
+	on:dragstart={() => handleDragStart(sq)}
+	on:dragover|preventDefault
+	on:drop|preventDefault={handleDrop}
+	id={`${squareId} ${sq.piece}`}
+	on:click|preventDefault={() => handleClick(sq)}
+	on:touchstart|passive={(event) => handleTouchStart(event, sq)}
+	on:touchmove|preventDefault={handleTouchMove}
+	on:touchend|passive={handleTouchEnd}
 >
-  <Piece {sq} />
+	<Piece {sq} />
 </button>
+
+<style>
+	.selected {
+		@apply bg-cyan-800;
+	}
+	.possible_move {
+		@apply bg-cyan-900;
+	}
+</style>
--- frontend/src/lib/components/chess/chessboard.svelte
+++ frontend/src/lib/components/chess/chessboard.svelte
@@ -5,7 +5,7 @@ import {
 	legalMove,
 } from "$lib/components/chess/utils";
 import type { Chess, PossibleMoves, Square as SquareType } from "../../types";
-import Piece from "./piece.svelte";
+import ChessSquare from "./chessSquare.svelte";
 
 export let chess: Chess;
 export let handleMove: (startSq: string, endSq: string) => Promise<void>;
@@ -109,129 +109,19 @@ const handleTouchEnd = async (_event: TouchEvent) => {
 	<div class="grid grid-cols-8 gap-0">
 		{#each boardToFront as row, i}
 			{#each row as sq, j}
-				{#if chess.white_player.in_check && typeof sq.piece === 'object' && sq.piece.King !== undefined && sq.piece.King === 'White'}
-					<button
-						class="h-11 w-11 sm:h-14 sm:w-14 md:h-16 md:w-16 lg:h-18 lg:w-18 xl:w-20 xl:h-20 bg-red-800 text-center hover:bg-red-900"
-						class:selected={selectedButton ===
-							sq.file.toLowerCase() + (sq.rank + 1)}
-						draggable="true"
-						on:dragstart={() => handleDragStart(sq)}
-						on:dragover|preventDefault
-						on:drop|preventDefault={handleDrop}
-						id={`${sq.file.toLowerCase()}${sq.rank + 1} white king in check`}
-						on:click|preventDefault={() => handleClick(sq)}
-						on:touchstart|passive={(event) => handleTouchStart(event, sq)}
-						on:touchmove|preventDefault={handleTouchMove}
-						on:touchend|passive={handleTouchEnd}
-					>
-						<Piece {sq} />
-					</button>
-				{:else if chess.black_player.in_check && typeof sq.piece === 'object' && sq.piece.King !== undefined && sq.piece.King === 'Black'}
-					<button
-						class="h-11 w-11 sm:h-14 sm:w-14 md:h-16 md:w-16 lg:h-18 lg:w-18 xl:w-20 xl:h-20 bg-red-800 hover:bg-red-900 text-center"
-						class:selected={selectedButton ===
-							sq.file.toLowerCase() + (sq.rank + 1)}
-						draggable="true"
-						on:dragstart={() => handleDragStart(sq)}
-						on:dragover|preventDefault
-						on:drop={handleDrop}
-						id={`${sq.file.toLowerCase()}${sq.rank + 1} black king in check`}
-						on:click|preventDefault={() => handleClick(sq)}
-						on:touchstart|passive={(event) => handleTouchStart(event, sq)}
-						on:touchmove|preventDefault={handleTouchMove}
-						on:touchend|passive={handleTouchEnd}
-					>
-						<Piece {sq} />
-					</button>
-				{:else if sq.color === 'White' && sq.piece !== 'None'}
-					<button
-						class="h-11 w-11 sm:h-14 sm:w-14 md:h-16 md:w-16 lg:h-18 lg:w-18 xl:w-20 xl:h-20 bg-gray-200 text-center hover:bg-gray-600"
-						class:selected={selectedButton ===
-							sq.file.toLowerCase() + (sq.rank + 1)}
-						class:possible_move={possibleMoves.some(
-							(move) => move[1][0] === j && move[1][1] === 8 - i,
-						)}
-						draggable="true"
-						on:dragstart={() => handleDragStart(sq)}
-						on:dragover|preventDefault
-						on:drop={handleDrop}
-						id={`${sq.file.toLowerCase()}${sq.rank + 1} ${sq.piece}`}
-						on:click|preventDefault={() => handleClick(sq)}
-						on:touchstart|passive={(event) => handleTouchStart(event, sq)}
-						on:touchmove|preventDefault={handleTouchMove}
-						on:touchend|passive={handleTouchEnd}
-					>
-						<Piece {sq} />
-					</button>
-				{:else if sq.color === 'Black' && sq.piece !== 'None'}
-					<button
-						class="h-11 w-11 sm:h-14 sm:w-14 md:h-16 md:w-16 lg:h-18 lg:w-18 xl:w-20 xl:h-20 bg-gray-400 text-center hover:bg-gray-600"
-						class:selected={selectedButton ===
-							sq.file.toLowerCase() + (sq.rank + 1)}
-						class:possible_move={possibleMoves.some(
-							(move) => move[1][0] === j && move[1][1] === 8 - i,
-						)}
-						draggable="true"
-						on:dragstart={() => handleDragStart(sq)}
-						on:dragover|preventDefault
-						on:drop|preventDefault={handleDrop}
-						id={`${sq.file.toLowerCase()}${sq.rank + 1} ${sq.piece}`}
-						on:click|preventDefault={() => handleClick(sq)}
-						on:touchstart|passive={(event) => handleTouchStart(event, sq)}
-						on:touchmove|preventDefault={handleTouchMove}
-						on:touchend|passive={handleTouchEnd}
-					>
-						<Piece {sq} />
-					</button>
-				{:else if sq.color === 'White'}
-					<button
-						class="h-11 w-11 sm:h-14 sm:w-14 md:h-16 md:w-16 lg:h-18 lg:w-18 xl:w-20 xl:h-20 bg-gray-200 text-center hover:bg-gray-600"
-						class:selected={selectedButton ===
-							sq.file.toLowerCase() + (sq.rank + 1)}
-						class:possible_move={possibleMoves.some(
-							(move) => move[1][0] === j && move[1][1] === 8 - i,
-						)}
-						draggable="false"
-						on:dragover|preventDefault
-						on:drop|preventDefault={handleDrop}
-						id={`${sq.file.toLowerCase()}${sq.rank + 1} ${sq.piece}`}
-						on:click|preventDefault={() => handleClick(sq)}
-						on:touchstart|passive={(event) => handleTouchStart(event, sq)}
-						on:touchmove|preventDefault={handleTouchMove}
-						on:touchend|passive={handleTouchEnd}
-					>
-						<Piece {sq} />
-					</button>
-				{:else}
-					<button
-						class="h-11 w-11 sm:h-14 sm:w-14 md:h-16 md:w-16 lg:h-18 lg:w-18 xl:w-20 xl:h-20 bg-gray-400 text-center hover:bg-gray-600"
-						class:selected={selectedButton ===
-							sq.file.toLowerCase() + (sq.rank + 1)}
-						class:possible_move={possibleMoves.some(
-							(move) => move[1][0] === j && move[1][1] === 8 - i,
-						)}
-						draggable="false"
-						on:dragover|preventDefault
-						on:drop|preventDefault={handleDrop}
-						id={`${sq.file.toLowerCase()}${sq.rank + 1} ${sq.piece}`}
-						on:click|preventDefault={() => handleClick(sq)}
-						on:touchstart|passive={(event) => handleTouchStart(event, sq)}
-						on:touchmove|preventDefault={handleTouchMove}
-						on:touchend|passive={handleTouchEnd}
-					>
-						<Piece {sq} />
-					</button>
-				{/if}
+				<ChessSquare
+					{chess}
+					{sq}
+					{selectedButton}
+					{possibleMoves}
+					{handleClick}
+					{handleDragStart}
+					{handleDrop}
+					{handleTouchStart}
+					{handleTouchMove}
+					{handleTouchEnd}
+				/>
 			{/each}
 		{/each}
 	</div>
 </div>
-
-<style>
-	.selected {
-		@apply bg-cyan-800;
-	}
-	.possible_move {
-		@apply bg-cyan-900;
-	}
-</style>
