a4d6bd0016a547cc72e6131ad61e21b594b6408b
Kristjan Rajaniemi <kristjan.rajaniemi@gmail.com>
2025-12-06 16:02:28 +0200

    fix header github link

--- frontend/src/lib/components/header.svelte
+++ frontend/src/lib/components/header.svelte
@@ -1,9 +1,9 @@
 <script lang="ts"></script>
 
 <header
-    class="bg-surface-500/15 shadow-lg p-8 left-0 top-0 flex flex-col gap-4 sm:flex-row justify-between items-center w-full"
+    class="bg-surface-500/15 shadow-lg p-8 left-0 top-0 flex flex-col gap-4 sm:flex-row justify-between items-center w-full relative"
 >
-    <nav class="flex space-x-4 justify-center items-center">
+    <nav class="flex space-x-4 justify-center items-center sm:flex-1">
         <!-- <a
             href="/technology"
             class="hover:text-gray-200 hover:border-b-2 hover:border-gray-200"
@@ -15,8 +15,10 @@
 			>About me</a
 		> -->
     </nav>
-    <h1 class="absolute left-1/2 transform -translate-x-1/2 h1">Chess App</h1>
-    <div class="text-gray-600 text-xs flex flex-row">
+    <h1 class="h1 sm:absolute sm:left-1/2 sm:transform sm:-translate-x-1/2">
+        Chess App
+    </h1>
+    <div class="text-gray-600 text-xs flex flex-row sm:flex-1 sm:justify-end">
         <a
             href="https://github.com/varjovaras"
             target="_blank"
